a1d9255f8c7af18b55c682ed0ab129891e269568
David Landeros <david.landeros@gameloft.com>
2017-02-01 14:39:28 -0800

    some improvements

--- example5 - platform pseudo game/example.js
+++ example5 - platform pseudo game/example.js
@@ -1,32 +1,32 @@
-
-
 var game = (function() {
 
-	var engine = null;
+    var engine = null;
     var platforms = null;
-	var canvas = null;
-	var context = null;
+    var canvas = null;
+    var context = null;
 
-	var box_texture = null;
-	var rock_texture = null;
-	var player_texture = null;
-	var texture_loaded = 0;
-	var total_textures = 3;
+    var box_texture = null;
+    var rock_texture = null;
+    var player_texture = null;
+    var texture_loaded = 0;
+    var total_textures = 3;
 
-	var controls = {
-		right : false,
-		left  : false,
-		jump  : false,
-		is_jump_enabled : true,
-	};
+    var controls = {
+        right : false,
+        left  : false,
+        jump  : false,
+        is_jump_enabled : false,
+    };
+  
+  var contact = [];
 
-	var player = null;
-	var rocks  = [];
-	var boxes  = [];
+    var player = null;
+    var rocks  = [];
+    var boxes  = [];
 
-	var box_size =  32;
+    var box_size =  32;
 
-	var map = [
+    var map = [
                 [ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 ],
                 [ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 ],
                 [ 1,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0 ],
@@ -42,18 +42,28 @@ var game = (function() {
                 [ 1,0,3,0,0,1,1,1,1,0,0,1,1,0,0,0,0,0,0,0 ],
                 [ 1,0,0,0,2,1,1,1,1,0,0,0,0,0,0,0,0,0,0,0 ],
                 [ 1,1,1,1,1,1,1,1,1,0,0,1,1,1,1,1,1,1,1,1 ],
-	];
+    ];
 
-	var rows = 15, cols = 20;
+    var rows = 15, cols = 20;
+  
+  function isTouchingGround(activeContacts)
+  {
+      for(var c = 0; c < activeContacts.length; c++)
+      {
+          if(activeContacts[c].vertex.y < player.position.y)
+              return false;
+      }
+      return true;
+  }  
 
-	function init(container_selector)
-	{
-		box_texture = new Image();
-		box_texture.src = "http://opengameart.org/sites/default/files/styles/medium/public/RTS_Crate_0.png";
-		rock_texture = new Image();
-		rock_texture.src = "http://hasgraphics.com/wp-content/uploads/2010/07/basic_ground_tileset.png";
-		player_texture = new Image();
-		player_texture.src = "http://cdn.wikimg.net/strategywiki/images/c/cc/Contra_NES_sprite_player1.png";
+    function init(container_selector)
+    {
+        box_texture = new Image();
+        box_texture.src = "http://opengameart.org/sites/default/files/styles/medium/public/RTS_Crate_0.png";
+        rock_texture = new Image();
+        rock_texture.src = "http://hasgraphics.com/wp-content/uploads/2010/07/basic_ground_tileset.png";
+        player_texture = new Image();
+        player_texture.src = "http://cdn.wikimg.net/strategywiki/images/c/cc/Contra_NES_sprite_player1.png";
 
         var div = document.querySelector(container_selector);
         canvas = document.createElement("canvas");
@@ -68,27 +78,30 @@ var game = (function() {
         {
             for(var j = 0; j < rows; j ++)
             {
-            	var type = map[j][i];
-            	var isStatic = type == 1;
-            	if(type != 0 && type != 1)
-            	{
+                var type = map[j][i];
+              var width = box_size;
+              if(type == 3)
+                  width = 15;
+                var isStatic = type == 1;
+                if(type != 0 && type != 1)
+                {
                      var obj = Matter.Bodies.rectangle(
-                     	i * box_size + (box_size >> 1),
-                     	j * box_size + (box_size >> 1),
-                     	box_size,
-                     	box_size,
-                     	{ isStatic: isStatic }
+                        i * box_size + (box_size >> 1),
+                        j * box_size + (box_size >> 1),
+                        width,
+                        box_size,
+                        { isStatic: isStatic,  friction: 1.0 }
                      );
                      switch(type)
                      {
-                     	case 1 : rocks.push(obj); break;
-                     	case 2 : boxes.push(obj); break;
-                     	case 3 : player = obj;
+                        case 1 : rocks.push(obj); break;
+                        case 2 : boxes.push(obj); break;
+                        case 3 : player = obj;
                                  player.friction = 0;
-                     	         Matter.Body.setInertia(obj, Infinity);
-                     	         break;
+                                 Matter.Body.setInertia(obj, Infinity);
+                                 break;
                      }
-            	}
+                }
             }
         }
 
@@ -110,81 +123,110 @@ var game = (function() {
         Matter.World.add(engine.world, [player]);
 
         window.addEventListener("keydown", function(e){
-        	handleKeyEvent(e.keyCode, true);
+            handleKeyEvent(e.keyCode, true);
         });
 
         window.addEventListener("keyup", function(e){
-        	handleKeyEvent(e.keyCode, false);
+            handleKeyEvent(e.keyCode, false);
         });
+        
+    Matter.Events.on(engine, 'collisionStart', function(event) {
+        var pairs = event.pairs;
+
+        // change object colours to show those starting a collision
+        for (var i = 0; i < pairs.length; i++) {
+            var pair = pairs[i];
+            if(pair.bodyA.id == player.id)
+            {
+                contact = [];
+                for(var c = 0; c < pair.activeContacts.length; c++)
+                {
+                    contact.push(pair.activeContacts[c].vertex);
+                }
+                
+                if(isTouchingGround(pair.activeContacts))
+                    controls.is_jump_enabled = true;
+                console.log(pair.activeContacts);
+                //controls.is_jump_enabled = true;
+                console.log("player touched the ground");
+            }
+            if(pair.bodyB.id == player.id)
+            {
+                controls.is_jump_enabled = true;
+                console.log("player touched the ground");
+            }
+        }
+    });        
 
         Matter.Engine.run(engine);
         requestAnimationFrame(update);
-	}
+    }
 
-	function handleKeyEvent(key, value)
-	{
-		switch(key)
-		{
-			case 65 : controls.left = value; break;
-			case 68 : controls.right = value; break;
-			case 87 : controls.jump = value; break;
-		}
-	}
+    function handleKeyEvent(key, value)
+    {
+        switch(key)
+        {
+            case 65 : controls.left = value; break;
+            case 68 : controls.right = value; break;
+            case 87 : controls.jump = value; break;
+        }
+    }
 
-	function update()
-	{
-		if(controls.right)
-		{
-			//Matter.Body.setVelocity(player, { x : 2, y : player.velocity.y } );
-			Matter.Body.translate(player, {x: 2, y: 0});
-		}
-		else if(controls.left)
-		{
+    function update()
+    {
+        if(controls.right)
+        {
+            //Matter.Body.setVelocity(player, { x : 2, y : player.velocity.y } );
+            Matter.Body.translate(player, {x: 2, y: 0});
+        }
+        else if(controls.left)
+        {
             //Matter.Body.setVelocity(player, { x : -2, y : player.velocity.y } );
             Matter.Body.translate(player, {x: -2, y: 0});
-		}
+        }
+        else
+        {
+            Matter.Body.setVelocity(player, { x : 0, y : player.velocity.y } );
+        }
         if(controls.jump && controls.is_jump_enabled)
         {
             controls.is_jump_enabled = false;
             Matter.Body.setVelocity(player, { x : player.velocity.x , y : -8} );
-            setTimeout(function(){
-              controls.is_jump_enabled = true;
-            }, 500);
         }
         //Matter.Body.rotate(player, -player.angle);
-		//Matter.Engine.update(engine, 15);
+        //Matter.Engine.update(engine, 15);
         render();
         requestAnimationFrame(update);
-	}
+    }
 
-	function render()
-	{
+    function render()
+    {
         context.clearRect(0,0, canvas.width, canvas.height);
 
         for(var i = 0; i < cols; i++)
         {
-        	for(var j = 0; j < rows; j++)
-        	{
-        		if(map[j][i] == 1)
-        		{
-        			var x = i * box_size;
-        			var y = j * box_size;
-        			var grass = (map[Math.max(j-1,0)][i] == 1);
-        			context.save();
+            for(var j = 0; j < rows; j++)
+            {
+                if(map[j][i] == 1)
+                {
+                    var x = i * box_size;
+                    var y = j * box_size;
+                    var grass = (map[Math.max(j-1,0)][i] == 1);
+                    context.save();
                     context.translate(x, y);
                     if(grass)
-                    	context.drawImage(rock_texture,25, 76, 50, 49, 0 , 0, box_size, box_size ); 
+                        context.drawImage(rock_texture,25, 76, 50, 49, 0 , 0, box_size, box_size ); 
                     else
-                    	context.drawImage(rock_texture,25, 15, 50, 50, 0 , 0, box_size, box_size );
+                        context.drawImage(rock_texture,25, 15, 50, 50, 0 , 0, box_size, box_size );
                     context.restore();
-        		}
-        	}
+                }
+            }
         }
 
         var l = boxes.length;
         for(i = 0; i < l; i++)
         {
-        	var b = boxes[i];
+            var b = boxes[i];
             context.save();
             context.translate(b.position.x, b.position.y);
             context.rotate(b.angle);
@@ -195,13 +237,14 @@ var game = (function() {
         context.save();
         context.translate(player.position.x, player.position.y);
         context.rotate(player.angle);
-        context.drawImage(player_texture, (-box_size>>1) , (-box_size>>1), box_size, box_size );
+        context.drawImage(player_texture, (-box_size>>1) + 8 , (-box_size>>1), box_size, box_size );
         context.restore();
         debug_render(platforms);
-	}
+    }
 
     function debug_render(platforms)
     {
+        //return;
         context.save();
         for(var p = 0; p < platforms.length; p++)
         {
@@ -219,8 +262,13 @@ var game = (function() {
             context.stroke();
         }
         context.restore();
+        context.save();
+        context.fillStyle = "white";
+        for(var c = 0; c < contact.length; c++)
+            context.fillRect(contact[c].x-2,contact[c].y-2,5,5);
+        context.restore();
     }
 
     return { init           : init,
-    	     handleKeyEvent : handleKeyEvent };
+             handleKeyEvent : handleKeyEvent };
 })();
